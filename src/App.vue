7eac617c80ac344ad0b63b7b7a49c05004877ac8
Daniel Baldeviano <edjbaldeviano@hotmail.com>
2026-06-15 12:16:38 +0800

    feat: wire up SoaForm into App and add full CSS styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <h1>SOA Generator</h1>
+    <header class="app-header">
+      <h1>SOA Generator</h1>
+      <p>Fill in the form below, then click <strong>Generate DOCX</strong> to download.</p>
+    </header>
+    <SoaForm />
   </div>
 </template>
 
 <script setup>
+import SoaForm from './components/SoaForm.vue'
 </script>
